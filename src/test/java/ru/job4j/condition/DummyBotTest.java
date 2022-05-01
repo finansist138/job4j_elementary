492af3cb890c6571f1777870b8479c4140743609
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-05-02 00:36:24 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        String in = "Привет, Бот.";
+        String result = DummyBot.answer(in);
+        String expected = "Привет, умник.";
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Пока.";
+        String result = DummyBot.answer(in);
+        String expected = "До скорой встречи.";
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "Сколько будет 2 + 2?";
+        String result = DummyBot.answer(in);
+        String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
+        Assert.assertEquals(result, expected);
+    }
+}
