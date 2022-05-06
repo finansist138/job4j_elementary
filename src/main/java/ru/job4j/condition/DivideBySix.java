a8581858f50ac8e4de5cd598fccde353d6a06e11
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-05-06 13:26:45 +0300

    4.4. Вложенный оператор if else. Запутанный код [#190 #297867]

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -2,18 +2,17 @@ package ru.job4j.condition;
 
 public class DivideBySix {
     public static String checkNumber(int number) {
-        String rsl = "";
+        String rsl;
         if (number % 3 == 0 && number % 2 == 0) {
             rsl = "Исходное число делится на 6.";
-        } else if (number % 3 == 0 && number % 2 == 1) {
+        } else if (number % 3 == 0) {
             rsl = "Исходное число делится на 3, но не является четным.";
-        } else if (number % 3 != 0 && number % 2 == 0) {
+        } else if (number % 2 == 0) {
             rsl = "Исходное число не делится на 3, но является четным.";
-        } else if (number % 3 != 0 && number % 2 == 1) {
+        } else {
             rsl = "Исходное число не делится на 3 и не является четным.";
         }
         return rsl;
     }
 }
-
 
