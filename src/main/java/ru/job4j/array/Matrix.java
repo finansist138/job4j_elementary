b50a9307635f35de0d9cc14ebbbd34ff36a6753d
finansist138 <103438132+finansist138@users.noreply.github.com>
2022-05-29 12:05:46 +0300

    6.6.8. Двухмерный массив. Таблица умножения. [#33491 #306260]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -3,10 +3,9 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] data = new int[size][size];
-        for (int row = 0; row < data.length; row++) {
-            for (int cell = 0; cell < data.length; cell++) {
-                int index = (row + 1) * (cell + 1);
-                data[row][cell] = index;
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                data[row][cell] = (row + 1) * (cell + 1);
             }
         }
         return data;
